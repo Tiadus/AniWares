8591dfa8155ca0e1a4ad9d69ef697bea37c63160
Viet Thai <vietthai.nguyen03@gmail.com>
2024-01-22 09:29:54 +0700

    [frontend] style: Styling home page footer.

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,8 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import DisplayBoard from "../components/DisplayBoard";
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Footer from '../components/Footer';
 
 const Shop = () => {
     return (
@@ -15,23 +14,8 @@ const Shop = () => {
                     <DisplayBoard /> 
                 </div>
             </Container>
-            <div style={{width: "100%", textAlign: "center", background:"red", paddingTop: "1%", marginTop: "4%"}}>
-                <Container style={{width: "100%", textAlign: "center"}}>
-                    <Row style={{marginTop: "10px", alignItems: 'center', justifyContent: 'center'}}>
-                        <Col lg={3} md={12} sm={12}>
-                            Service
-                        </Col>
-                        <Col lg={3} md={12} sm={12}>
-                            About
-                        </Col>
-                        <Col lg={3} md={12} sm={12}>
-                            Contact
-                        </Col>
-                        <Col lg={3} md={12} sm={12}>
-                            Facebook
-                        </Col>
-                    </Row>
-                </Container>
+            <div style={{width: "100%", textAlign: "center", background:"#333333", paddingTop: "1%", marginTop: "4%"}}>
+                <Footer />
             </div>
         </div>
     );
