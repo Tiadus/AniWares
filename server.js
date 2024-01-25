815dee5341434b4146ce9223d1e74111778b20b8
Viet Thai <vietthai.nguyen03@gmail.com>
2024-01-25 17:16:25 +0700

    [backend] feat: set up connection with MySQL

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
+//Import
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.get("/message", function(req,res) {
-    res.json({message: "Hello World"});
+//Required for body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//Get root
+app.get('/', function(req, res) {
+    res.send("Welcome to the root path!");
 });
+
+//Create connection to mysql server
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'ngaymai123',
+    database: 'AniWares',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
 
 app.listen(4000, function() {
     console.log("Listening on port 4000");
