736acd435ae9541e46927d429226a932efe1d70f
Viet Thai <vietthai.nguyen03@gmail.com>
2024-01-26 17:24:05 +0700

    [frontend] refractor: Refractor Shop
    Let Shop directly include Home component without wrap

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -2,24 +2,61 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Container from 'react-bootstrap/Container';
 import Footer from '../components/Footer';
 import MainNavBar from "../components/NavBar";
-import Home from "../components/Home";
 import { useState, useEffect } from "react";
+import DisplayBoard from "../components/DisplayBoard";
+import "./Shop.css"
+import Banner from "../components/Banner";
+import axios from "axios";
 
 const Shop = () => {
-    const [shopController, setShopController] = useState(
+    const [homeData, setHomeData] = useState(
         {
-            home: true,
-            search: false,
-            cart: false,
-            account: false
+            discountItems: []
         }
-    );
+    )
+
+    useEffect(() => {
+        fetchHomeData();
+    }, []);
+
+    const fetchHomeData = async () => {
+        let discountItemRequest = {
+            params: {
+                search: "",
+                brandValue: "",
+                categoryValue: "",
+                typeValue: "",
+                scaleValue: "",
+                seriesValue: "",
+                nameValue: "",
+                minPriceValue: 0,
+                maxPriceValue: 1000000,
+                discountValue: true,
+                statusValue: ""
+            }
+        }
+
+        let discountResponse = await axios.get("/item", discountItemRequest);
+        let discountItems= discountResponse.data;
+        setHomeData({
+            ...homeData,
+            discountItems: discountItems
+        })
+    };
 
     return (
-        <div style={{width: "100%", height: "100%"}}>
+        <div>
             <MainNavBar />
-            <div style={{height: "100%", marginTop: "3%"}}>
-                {shopController.home === true && <Home />}
+            <div style={{marginTop: "3%"}}>
+                <Container style={{marginBottom: "4%"}}>
+                    <Banner/>
+                </Container>
+                <div className="captionHolder">
+                    <div className="col-sm-4 col-md-4 col-lg-4 caption">On Sale</div>
+                </div>
+                <Container className="boardHolder">
+                    <DisplayBoard items={homeData.discountItems}/> 
+                </Container> 
             </div>
             <div style={{width: "100%", textAlign: "center", background:"#333333", paddingTop: "1%", marginTop: "4%", paddingBottom: "2%"}}>
                 <Container>
