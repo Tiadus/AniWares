a62d8916da0e6d771edcb5d4ef3070e8e911303a
Viet Thai <vietthai.nguyen03@gmail.com>
2024-01-26 17:53:23 +0700

    [frontend] feat: Router & Navigation Update the app with browser router and NavBar with the ability to route to Search page with query value

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Shop from './pages/Shop';
+import Search from './pages/Search';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div style={{width: "100%", height: "100%"}}>
-      <Shop/>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element = {<Shop />} />
+          <Route path="/shop" element = {<Shop />} />
+          <Route path="/search" element = {<Search />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,9 +6,25 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function MainNavBar() {
   let expand = "lg";
+  const searchRef = useRef(null);
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = () => {
+    const query = searchRef.current.value;
+    navigate(`/search?query=${query}`);
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const query = searchRef.current.value;
+    navigate(`/search?query=${query}`);
+  }
+
   return (
     <>
         <Navbar expand={expand} className="mb-3" style={{backgroundColor: "#d3d3d3"}}>
@@ -48,14 +64,14 @@ function MainNavBar() {
                     </NavDropdown.Item>
                   </NavDropdown>
                 </Nav>
-                <Form className="d-flex">
-                  <Form.Control
-                    type="search"
+                <Form className="d-flex" onSubmit={handleFormSubmit}>
+                  <Form.Control type="text"
+                    ref={searchRef}
                     placeholder="Search"
                     className="me-2"
                     aria-label="Search"
                   />
-                  <Button variant="outline-success" >Search</Button>
+                  <Button variant="outline-success" onClick={handleSearchSubmit}>Search</Button>
                 </Form>
                 <Nav className="justify-content-end flex-grow-1">
                   <Nav.Link href="#action1">Cart</Nav.Link>
