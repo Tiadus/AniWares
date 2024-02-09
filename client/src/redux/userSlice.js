53555f85f0d563e4a6017ca7c26e17a4907214a7
Viet Thai <vietthai.nguyen03@gmail.com>
2024-02-09 22:46:17 +0700

    [frontend] feat(redux): isAdmin Attribute
    The isAdmin attribute in state is used to
    determined whether admin mode should be activated.

--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -3,15 +3,20 @@ import { createSlice } from "@reduxjs/toolkit";
 export const userSlice = createSlice({
     name: 'user',
     initialState: {
-        userCode: 0
+        userCode: 0,
+        isAdmin: 0
     },
     reducers: {
         setUserCode: (state, action) => {
             state.userCode = action.payload;
-        }   
+        },
+        setUserPrivilege: (state, action) => {
+            state.isAdmin = action.payload;
+        }
     }
 })
 
 export const {setUserCode} = userSlice.actions;
+export const {setUserPrivilege} = userSlice.actions;
 
 export default userSlice.reducer;
